c4dd4d8a05a9193cd901f3a05c423c59cafff6a8
mats cronqvist <masse@cronqvi.st>
2015-12-01 21:07:45 +0100

    ch2

--- ch2/src/Main.purs
+++ ch2/src/Main.purs
@@ -1,5 +1,12 @@
 module Main where
 
+import Prelude
+import Math (sqrt)
+import Math (pi)
 import Control.Monad.Eff.Console
 
-main = log "Hello, World!"
+diagonal w h = sqrt (w*w + h*h)
+
+circle_area r = pi*r*r
+
+main = print (diagonal 3.0 4.0)
